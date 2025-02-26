493f5382535b07c0d69cec569a98f759c3e9561c
Jyystaja <iisak.merilehto@gmail.com>
2025-02-26 22:11:35 +0200

    ads

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/turbonetti",
+  base: "/turbonetti/",
   server: {
     host: "::",
     port: 8080,
